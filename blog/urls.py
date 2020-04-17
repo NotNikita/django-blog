13e12f13c18d5740e13fb08e0c25b67345c04519
Nikita <nikitabsuir@gmail.com>
2020-04-17 20:40:57 +0300

    added urls

--- blog/urls.py
+++ blog/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 from . import views
 
 
-#URL-шаблон
+#URL-шаблон 
+#мы связали view под именем post_list с корневым URL-адресом ('')
 urlpatterns = [
 	path('', views.post_list, name = 'post_list'),
 ]
